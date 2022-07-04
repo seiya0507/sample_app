610867df92f1f3d887e16ef44a9a0c6830e4a4b8
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-07-04 13:54:39 +0000

    [Finish] 2-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,19 +11,27 @@ class ListsController < ApplicationController
 
 
   def index
-     @lists = List.all
+    @lists = List.all
   end
 
   def show
-     @list = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end  
 
   private
 
   def list_params
     params.require(:list).permit(:title, :body)
   end
+  
 end
