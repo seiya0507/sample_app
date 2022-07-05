b21f26b0924c172c8641288f7a57b6a0204f7173
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-07-05 07:59:08 +0000

    [Finish] 3-11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get '/top' => 'homes#top'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   resources :lists
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
